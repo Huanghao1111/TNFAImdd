028db3d1c0b0ead7af80548d4c14cf897138485c
Haozhang Huang <135173750+Huanghao1111@users.noreply.github.com>
2026-01-19 15:38:25 +0800

    Create 104.negative_control_mr_analysis.R

---
+++ 104.negative_control_mr_analysis.R
@@ -0,0 +1,2 @@
+mr_common("TNFI", "CHD.csv", no_clump = TRUE, steiger = FALSE, p2 = 0) # Congenital malformations of the heart and great arteries (finn-b-CONGEN_HEART_ARTER)
+mr_common("TNFI", "HD.csv", no_clump = TRUE, steiger = FALSE, p2 = 0) # Hereditary deficiency of other clotting factors (finn-b-D3_HEREDOTHCLOFACTORS)
