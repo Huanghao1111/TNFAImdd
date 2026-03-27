6eb70d7c0d1496fdbbb935763e0e646f23d12056
Haozhang Huang <135173750+Huanghao1111@users.noreply.github.com>
2026-03-27 08:36:35 +0800

    Update 101.core_mr_analysis.R

--- 101.core_mr_analysis.R
+++ 101.core_mr_analysis.R
@@ -1,4 +1,6 @@
 # TNF-mediated CRP
+# total Sample size of exposure = 343524
+# total sample size cases of outcome = 78868~357636; total sample size controls of outcome = 461528~1281936; total Sample size of outcome = 541378~1639572; population prevalence of outcome = 12.1%~30.6%
 mr_common("ukb-d-30710_irnt.csv", "MDD2025_PGC_no23andMe_noUKBB_eur.csv", p1=5e-8, p2=0,
           local_clump = TRUE, r2 = 0.3, kb = 100, gene = "TNF", steiger = T,
           eaf_threshold = 0.01, gene_win = 100)
@@ -8,6 +10,8 @@ mr_common("ukb-d-30710_irnt.csv", "MDD2025_PGC_no23andMe_noUKBB_eur.csv", p1=5e-
 mr_common("TNF_CRP.csv", "MDD2025_PGC_no23andMe_noUKBB_eur.csv", no_clump = TRUE)
 
 # TNF-mediated WBC
+# total Sample size of exposure = 350470
+# total sample size cases of outcome = 78868~357636; total sample size controls of outcome = 461528~1281936; total Sample size of outcome = 541378~1639572; population prevalence of outcome = 12.1%~30.6%
 mr_common("ukb-d-30000_irnt.csv", "MDD2025_PGC_no23andMe_noUKBB_eur.csv", p1=5e-8, p2=0,
           local_clump = TRUE, r2 = 0.3, kb = 100, gene = "TNF",steiger = T,
           eaf_threshold = 0.01, gene_win = 100)
