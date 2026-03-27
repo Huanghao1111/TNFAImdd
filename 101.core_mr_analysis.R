4e86820f188b2ff02514e97d0c822b3402db2592
Haozhang Huang <135173750+Huanghao1111@users.noreply.github.com>
2026-03-27 08:16:35 +0800

    Add steiger parameter to mr_common calls

--- 101.core_mr_analysis.R
+++ 101.core_mr_analysis.R
@@ -1,6 +1,6 @@
 # TNF-mediated CRP
-mr_common("ukb-d-30710_irnt.csv", "MDD2025_PGC_no23andMe_noUKBB_eur.csv",
-          local_clump = TRUE, r2 = 0.3, kb = 100, gene = "TNF",
+mr_common("ukb-d-30710_irnt.csv", "MDD2025_PGC_no23andMe_noUKBB_eur.csv", p1=5e-8, p2=0,
+          local_clump = TRUE, r2 = 0.3, kb = 100, gene = "TNF", steiger = T,
           eaf_threshold = 0.01, gene_win = 100)
 
 # TNF_CRP.csv contains the extracted SNPs.
@@ -8,14 +8,14 @@ mr_common("ukb-d-30710_irnt.csv", "MDD2025_PGC_no23andMe_noUKBB_eur.csv",
 mr_common("TNF_CRP.csv", "MDD2025_PGC_no23andMe_noUKBB_eur.csv", no_clump = TRUE)
 
 # TNF-mediated WBC
-mr_common("ukb-d-30000_irnt.csv", "MDD2025_PGC_no23andMe_noUKBB_eur.csv",
-          local_clump = TRUE, r2 = 0.3, kb = 100, gene = "TNF",
+mr_common("ukb-d-30000_irnt.csv", "MDD2025_PGC_no23andMe_noUKBB_eur.csv", p1=5e-8, p2=0,
+          local_clump = TRUE, r2 = 0.3, kb = 100, gene = "TNF",steiger = T,
           eaf_threshold = 0.01, gene_win = 100)
 
 # TNF_WBC.csv contains the extracted SNPs.
 # To indicate the direction of TNF-mediated WBC, the beta values were multiplied by −1.
-mr_common("TNF_WBC.csv", "MDD2025_PGC_no23andMe_noUKBB_eur.csv", no_clump = TRUE)
+mr_common("TNF_WBC.csv", "MDD2025_PGC_no23andMe_noUKBB_eur.csv", steiger = T, no_clump = TRUE)
 
 # Merge the two extracted SNP files.
 # For duplicated SNPs, the SNP with the larger F-statistic was retained.
-mr_common("TNFI", "MDD2025_PGC_no23andMe_noUKBB_eur.csv", no_clump = TRUE)
+mr_common("TNFI", "MDD2025_PGC_no23andMe_noUKBB_eur.csv", steiger = T, no_clump = TRUE)
